3a70e6da9fb206db53ed3f8acba31e1eb8e85f6d
David E. Piedra <davidpiedra@ccs.k12.in.us>
2022-01-14 18:14:39 -0500

    work

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
   "os"
   "github.com/gorilla/mux"
+  "net/http"
   "html/template"
 )
 
@@ -22,4 +23,14 @@ func main() {
   router := newRouter()
   port := ":80"
   http.ListenAndServer(port, router)
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+  w.Header().Set("Content-Type","text/html")
+  tmpl, err := template.ParseFiles("templates/pregame.html")
+  if err != nil {
+    http.Error(w, err.Error(), http.StatusInternalServerError)
+    return
+  }
+  tmpl.Execute(w, map[string]string{})
 }
